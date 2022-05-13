7f3dfa609e814c053c72662cc0e198ccf824c122
User <abdurasilov99@gmail.com>
2022-05-13 12:58:49 +0600

    HomeWorkSpring9

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -7,8 +7,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+
+        Cat cat1 = applicationContext.getBean("cat",Cat.class);
+        System.out.println(cat1.getMessage());
+        Cat cat2 = applicationContext.getBean("cat",Cat.class);
+        System.out.println(cat2.getMessage());
+        System.out.println(cat1==cat2);
     }
 }
